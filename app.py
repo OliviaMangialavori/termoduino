87db463e76c606fd1e94df08d615f872609548dc
theo Patinio <theo@frika.com.ar>
2021-12-02 20:50:54 -0300

    Implementando Mongo

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://app:1HGbaYcRmUr3Rgoq@cluster0.yph9l.mongodb.net/Termoduino?retryWrites=true&w=majority"
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello_world():
   return "<h1>Aplicacion corriendo en puerto 5000</h1>"
+
+@app.route("/get_temp")
+def get_temp():
+  temperaturas = mongo.db.Temperaturas.find()
+  for x in temperaturas:
+    print(x)
+  return temperaturas
 
 @app.route("/save_temp/<temp>")
 def save_temp(temp):
